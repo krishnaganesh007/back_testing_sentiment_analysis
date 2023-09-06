a4780be780a4fc79f7a72373ea0afb4723e4d588
Ganesh Yeluri <krishnaganesh007@Ganeshs-MacBook-Pro.local>
2023-09-06 18:37:37 +0530

    added reddit codes and data

--- PRAW.py
+++ PRAW.py
@@ -11,13 +11,32 @@ response = requests.post("https://www.reddit.com/api/v1/access_token", auth=clie
 response.json()
 
 
+import pandas as pd
+
 import praw
 reddit = praw.Reddit(client_id='', client_secret='', user_agent='krishnaScrapes')
 
 
-hot_posts = reddit.subreddit('MachineLearning').hot(limit=10)
+hot_posts = reddit.subreddit('elonmusk').hot(limit=100)
+
+reddit_df = pd.DataFrame()
 for post in hot_posts:
-    print(post.title)
+
+    comment_list = []
+    for comment in post.comments:
+        #print(comment.body)
+        comment_list.append(comment.body)
+    
+    tmp_df = pd.DataFrame()
+    tmp_df["comments_list"] = comment_list
+    tmp_df["post_id"] = post.id
+    tmp_df["post_title"] = post.title
+    
+
+    reddit_df = pd.concat([reddit_df, tmp_df], axis = 0 )
+
+reddit_df = reddit_df.reset_index(drop = True)
+reddit_df.to_csv("./reddit_tesla_posts_comments_top100.csv", index = False)
 
 hot_posts = reddit.subreddit('all').hot(limit=10)
 for post in hot_posts:
